389f819430e53e20b5411227bf8c2651eb2b59f5
Claudiu-Iulian Furtea <claudiufurtea@gmail.com>
2023-02-12 15:01:11 +0200

    Code for my website

---
+++ demo/contact.php
@@ -0,0 +1,25 @@
+<?php include('includes/header.php') ?>
+
+      <div class="container py-3">
+        <h1>Contact</h1>
+        <div class="row justify-content-center">
+            <div class="col-6">
+               Telefon: <a href="">0758353914</a> <br>
+               Email: <a href="claudiufurtea@gmail.com">claudiufurtea@gmail.com</a><br>
+               Adresa: Strada Alexandru Sahia 17, Râșnov Romacril 505400 <hr>
+               <h5>Manager de vanzari</h5>
+               <strong>Jeff Bezos</strong><br>
+               Email: <a href="">jeffbezos@amazon.us</a> <hr>
+               <h5>Relatii cu publicul</h5>
+               <strong>Negrea Paula Beatrice</strong> <br>
+               Email: <a href="">negreapaulabeatrice@gmail.com</a>
+            </div>
+            <div class="col-6">
+              <div class="mx-4">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1578.3440270685164!2d25.452777850619203!3d45.58940268173973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b34596f25e7f0f%3A0xa40a5baab03ba0f8!2sStrada%20Alexandru%20Sahia%2017%2C%20R%C3%A2%C8%99nov%20Romacril%20505400!5e0!3m2!1sro!2sro!4v1665229413307!5m2!1sro!2sro" width="500" height="350" style="border:0; padding-left: 20px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+              </div>
+            </div>
+        </div>
+      </div>
+      
+<?php include('includes/footer.php') ?>
---
+++ demo/despre-noi.php
@@ -0,0 +1,59 @@
+<?php include('includes/header.php') ?>
+
+    <div class="container py-3">
+        <h1>Despre noi</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor suscipit mauris in tincidunt. Donec sed lorem sit amet tellus bibendum aliquet. In hac habitasse platea dictumst. Phasellus magna est, commodo nec fermentum ut, fringilla sed lacus. Pellentesque porta sapien turpis, ut mattis elit mattis a. Duis in semper felis. Etiam vestibulum nibh tempor nisl accumsan, sit amet lacinia orci consequat. Quisque aliquam gravida risus, a viverra nisl ullamcorper ut.</p>
+        <p>Aenean elementum, urna molestie scelerisque placerat, lectus erat scelerisque arcu, nec volutpat tellus urna sed eros. Morbi consequat libero at nunc hendrerit mollis. Mauris a lacus eget est euismod rhoncus. Nunc ex lectus, interdum vel quam vitae, accumsan faucibus nisi. Nulla risus velit, dapibus sit amet vehicula vitae, lobortis at ipsum. Mauris ultrices, ipsum vitae volutpat porta, mi ante consectetur nisi, fermentum dignissim metus sapien ac arcu.</p>
+        <p>
+            <button class="btn btn-outline-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+              Citeste mai mult
+            </button>
+        </p>
+            <div class="collapse" id="collapseWidthExample">
+              <div class="card card-body" style="width: 100%;">
+                <p> Commodo sem sit amet metus posuere ultricies. In lacus lectus, vehicula tristique risus id, vulputate varius dui. Pellentesque nec odio mollis, tempor tellus eget, fringilla tortor. Quisque sed gravida tellus, at porta nulla. In hac habitasse platea dictumst.</p>
+                <p> Cras sollicitudin interdum felis, sit amet commodo est bibendum in. In vel nunc risus. Aliquam metus augue, tempor at laoreet non, posuere non elit. Donec pharetra fermentum ultricies. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus condimentum lobortis metus, in aliquam magna consequat eget. Morbi accumsan interdum vestibulum. Suspendisse potenti.</p> 
+                <p> Nullam elementum bibendum leo, ut finibus nunc consequat ultricies. Nullam pellentesque sed lectus id commodo. Donec hendrerit sodales dolor, auctor fermentum tellus dictum sed. Suspendisse metus dui, dictum in sem ut, sodales dapibus ipsum. In ultrices augue ut imperdiet vehicula. Proin metus nulla, fringilla rutrum interdum et, ullamcorper at velit.</p>
+              </div>
+            </div>
+          </div>
+    </div>
+
+ <div class="echipa bg-secondary py-5">
+    <div class="container">
+        <h2>Echipa</h2>
+     <div class="row">
+        <div class="col-4">
+            <div class="card">
+                <img src="https://cdn.vox-cdn.com/thumbor/3LVq6mg6nxSo5wjAoE8Fil83hXc=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/10629619/victor_trust_zuck_getty_ringer.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Mark Zuckerberg</h5>
+                  <p class="card-text">CEO</p>
+                </div>
+              </div>
+            </div>
+        
+        <div class="col-4">
+            <div class="card">
+                <img src="https://pyxis.nymag.com/v1/imgs/64c/5f5/aa7bad8c89acf39949cea29f2218bb8da5-26-jeff-bezos.rsquare.w700.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Jeff Bezos</h5>
+                  <p class="card-text">Manager vanzari</p>
+                </div>
+              </div>
+        </div>
+        
+        <div class="col-4">
+            <div class="card">
+                <img src="https://cdn.thegentlemansjournal.com/wp-content/uploads/2015/10/SteveJobsSquare-TGJ.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Steve Jobs</h5>
+                  <p class="card-text">Actionar principal</p>
+                </div>
+              </div>
+        </div>
+      </div>
+    </div>
+ </div>
+
+ <?php include('includes/footer.php') ?>
